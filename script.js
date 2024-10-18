06fcde7601b5aa8fce5b129ac7d1c7cd9747b762
Massimiliano <maxtera87@gmail.com>
2024-10-18 17:56:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -111,7 +111,102 @@ function proceedBtn() {
 }
 
 let contatore = 0;
-let btn1 = document.getElementById
+let contatoreSbagliate = 0;
+let currentQuestionIndex = 0;
+
+let questionsRandomized = shuffle(questions)
+console.log(questionsRandomized)
+
+
+//FUNZIONE DI GESTIONE DOMANDE 
+function quizzone() {
+  if (currentQuestionIndex >= questionsRandomized.length) {
+    /*  window.location.href = linkFinale; */
+
+    let contatori = [contatore, contatoreSbagliate]
+    return contatori
+  }
+  let contatore = contatori[0]
+  let contatoreSbagliate = contatori[1]
+
+
+  let currentQuestion = questionsRandomized[currentQuestionIndex];
+  let answers = [
+    ...currentQuestion.incorrect_answers,
+    currentQuestion.correct_answer,
+  ];
+
+
+  answers = shuffle(answers);
+  console.log(answers)
+
+  document.getElementById("question").innerText = currentQuestion.question;
+  let buttonContainer = document.getElementById("contenitoreButtoni");
+  buttonContainer.innerHTML = '';
+
+  for ( i = 0; i < answers.length; i++){
+    let buttone = document.createElement("button");
+    buttone.innerText = answers[i];
+    buttone.className = "Btn";
+
+    buttone.addEventListener("click", function() { checkAnswer(answers[i], currentQuestion.correct_answer);});
+
+    buttonContainer.appendChild(buttone);
+  }
+
+    /*let buttone = document.createElement("button");
+    buttone.innerText = "Conferma";
+    buttone.className = "Btn";
+    buttonContainer.appendChild(buttone);
+    buttone.addEventListener("click", function() { quizzone();});*/
+
+  /*btn1.innerText = answers[0];
+  btn2.innerText = answers[1];
+  btn3.innerText = answers[2]; 
+  btn4.innerText = answers[3];
+
+
+  btn1.onclick = () => checkAnswer(answers[0], currentQuestion.correct_answer);
+  btn2.onclick = () => checkAnswer(answers[1], currentQuestion.correct_answer);
+  btn3.onclick = () => checkAnswer(answers[2], currentQuestion.correct_answer);
+  btn4.onclick = () => checkAnswer(answers[3], currentQuestion.correct_answer);*/
+
+  currentQuestionIndex++;
+
+}
+
+
+function checkAnswer(selectedAnswer, correctAnswer) {
+  if (selectedAnswer === correctAnswer) {
+
+    contatore++;
+  } else {
+    contatoreSbagliate++;
+  }
+  console.log(contatore)
+  console.log(contatoreSbagliate)
+  quizzone()
+}
+
+
+quizzone();
+/*
+//PAGINA PRINCIPALE - FUNZIONE PER PROCEDERE ALLE DOMANDE
+let checkboxFlag = document.getElementById('checkboxFlag')
+let linkRiferimento = "./domPage.html";
+let linkFinale = "./paginaFinale.html"
+function proceedBtn() {
+  if (checkboxFlag.checked) {
+    window.location.href = linkRiferimento;
+
+  } else {
+    alert("Please check the checkbox to proceed.");
+  }
+}
+
+let risposteGiuste = [];
+
+
 
 function questionario() {
 
